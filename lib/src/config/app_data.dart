5c65d8da80ce62d55ae097ba11bf0e5ad4c95a0a
Rodrigo Hironaka <rocknhiro@hotmail.com>
2025-12-09 20:12:54 -0300

    Primeiro commit

---
+++ lib/src/config/app_data.dart
@@ -0,0 +1,66 @@
+import 'package:greengrocer/src/models/item_model.dart';
+
+ItemModel apple = ItemModel(
+  description: 'A melhor maçã da região e que conta com o melhor preço.',
+  imgUrl: 'assets/fruits/apple.png',
+  itemName: 'Maçã',
+  price: 5.5,
+  unit: 'kg',
+);
+
+ItemModel grape = ItemModel(
+  description: 'A melhor Uva da região e que conta com o melhor preço.',
+  imgUrl: 'assets/fruits/grape.png',
+  itemName: 'Uva',
+  price: 7.4,
+  unit: 'kg',
+);
+
+ItemModel guava = ItemModel(
+  description: 'A melhor goiaba da região e que conta com o melhor preço.',
+  imgUrl: 'assets/fruits/guava.png',
+  itemName: 'Goiaba',
+  price: 11.5,
+  unit: 'kg',
+);
+
+ItemModel kiwi = ItemModel(
+  description: 'A melhor kiwi da região e que conta com o melhor preço.',
+  imgUrl: 'assets/fruits/kiwi.png',
+  itemName: 'Kiwi',
+  price: 2.5,
+  unit: 'kg',
+);
+
+ItemModel mango = ItemModel(
+  description: 'A melhor manga da região e que conta com o melhor preço.',
+  imgUrl: 'assets/fruits/mango.png',
+  itemName: 'Manga',
+  price: 2.5,
+  unit: 'kg',
+);
+
+ItemModel papaya = ItemModel(
+  description: 'A melhor papaya da região e que conta com o melhor preço.',
+  imgUrl: 'assets/fruits/papaya.png',
+  itemName: 'Maçã',
+  price: 8,
+  unit: 'kg',
+);
+
+List<ItemModel> items = [
+  apple, 
+  grape, 
+  guava, 
+  kiwi,
+  mango, 
+  papaya,
+];
+
+List<String> categories = [
+    'Frutas',
+    'Grãos',
+    'Verduras',
+    'Temperos',
+    'Cereais',
+  ];
